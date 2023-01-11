f81717c08bd13c40ea70d8b9a4944a5aa239c054
Tae Hyeon Kim <122070432+Tae-Hyeon-Kim@users.noreply.github.com>
2023-01-11 16:20:00 +0900

    update_01.11

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,25 @@
-var store = []
+var store = [{
+        "title": "[Jekyll] 블로그 만들기",
+        "excerpt":"깃허브 블로그 개설 (chirpy 테마) 저는 블로그 개설하는데에 굉장히 어려움을 겪었는데 어떤 어려움을 겪었는지, 쉽게 하는 방법은 어떤 것인지 설명해보겠습니다! 누구나 그렇듯 저는 블로그를 만들어보겠다는 마음가짐으로 구글링을 통해 정보를 얻었는데, 대부분의 블로그에서 깃허브 repository 만드는 것을 첫 번째로 알려주는 경우가 많습니다. 하지만 저는 그렇게 하다가 어려움을 겪었기 때문에.. 우선 먼저...","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/first-post/",
+        "teaser": null
+      },{
+        "title": "[Dacon] 구내식당 식수 예측",
+        "excerpt":"데이콘 실무역량 강화교육을 통해 경험했던 첫 번째 프로젝트에 대한 코드입니다. 코랩을 사용하였고 간단한 EDA를 통해 데이터의 특성을 파악하고 그에 따른 모델을 만들어 식수인원을 예측해보았습니다. from google.colab import drive drive.mount('/content/drive') Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True). import pandas as pd import matplotlib.pyplot as plt...","categories": ["Dacon"],
+        "tags": ["python","Dacon","ml"],
+        "url": "/dacon/dacon-first/",
+        "teaser": null
+      },{
+        "title": "[Dacon] 와인 품질 분류 경진대회",
+        "excerpt":"데이콘 실무역량 강화교육 간에 실시한 프로젝트인 와인분류 프로젝트입니다. 성능 향상을 위해 앙상블기법을 활용해 보았습니다. 또한 하드보팅과 소프트 보팅에 대해 배웠습니다. 0.준비 1) 데이터 로드 from google.colab import drive drive.mount('/content/drive') Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True). import matplotlib.pyplot as plt import pandas as pd...","categories": ["Dacon"],
+        "tags": ["python","Dacon","ml"],
+        "url": "/dacon/dacon-wine-classifier/",
+        "teaser": null
+      },{
+        "title": "[Dacon] 유전체 정보 분류 경진대회",
+        "excerpt":"Import import pandas as pd import random import os import numpy as np import matplotlib.pyplot as plt from sklearn import preprocessing from sklearn.ensemble import RandomForestClassifier class CFG: SEED = 42 def seed_everything(seed): random.seed(seed) os.environ['PYTHONHASHSEED'] = str(seed) np.random.seed(seed) seed_everything(CFG.SEED) # Seed 고정 Data Load from google.colab import drive drive.mount('/content/drive') Mounted at...","categories": ["Dacon"],
+        "tags": ["python","Dacon","ml"],
+        "url": "/dacon/dacon-%EC%9C%A0%EC%A0%84%EC%B2%B4_%EC%A0%95%EB%B3%B4_%EB%B6%84%EB%A5%98_%EC%A0%95%EB%A6%AC/",
+        "teaser": null
+      }]
