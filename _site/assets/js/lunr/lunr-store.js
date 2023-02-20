0e96b58f1c7c9c2c443c0031dc0ebd30f9cd6059
Tae Hyeon Kim <122070432+Tae-Hyeon-Kim@users.noreply.github.com>
2023-02-20 14:43:04 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -40,4 +40,10 @@ var store = [{
         "tags": ["python","algorithm","dfs/bfs"],
         "url": "/programmers/programmers-network-solution/",
         "teaser": null
+      },{
+        "title": "[Programmers] 네트워크",
+        "excerpt":"프로그래머스 코팅테스트 연습문제를 푸는 과정에서 굉장히 깔끔하게 풀린 문제가 있어서 풀이과정을 공유하고자 작성하게 되었다! 1. 문제 문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/43162 먼저 입력은 컴퓨터의 개수 n 과 컴퓨터 간 연결 관계에 대한 리스트 computers 가 주어지고 두 정보를 이용하여 네트워크의 개수 즉, 컴퓨터가 몇 묶음으로 연결되어있는지를 출력해야 한다. 2. 접근 방법 이...","categories": ["Programmers"],
+        "tags": ["python","algorithm","dfs/bfs"],
+        "url": "/programmers/programmers-network-solution-test/",
+        "teaser": null
       }]
