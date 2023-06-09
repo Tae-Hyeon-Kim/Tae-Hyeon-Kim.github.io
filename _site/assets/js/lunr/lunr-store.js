c910f5ab2194dbc5c9ed827b6b3dc246874e06bf
Tae Hyeon Kim <122070432+Tae-Hyeon-Kim@users.noreply.github.com>
2023-06-09 16:11:35 +0900

    css 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,19 +6,19 @@ var store = [{
         "teaser": null
       },{
         "title": "[Dacon] 구내식당 식수 예측",
-        "excerpt":"데이콘 실무역량 강화교육을 통해 경험했던 첫 번째 프로젝트에 대한 코드입니다. 코랩을 사용하였고 간단한 EDA를 통해 데이터의 특성을 파악하고 그에 따른 모델을 만들어 식수인원을 예측해보았습니다. from google.colab import drive drive.mount('/content/drive') Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True). import pandas as pd import matplotlib.pyplot as plt...","categories": ["Dacon"],
+        "excerpt":"데이콘 실무역량 강화교육을 통해 경험했던 첫 번째 프로젝트에 대한 코드입니다. 코랩을 사용하였고 간단한 EDA를 통해 데이터의 특성을 파악하고 그에 따른 모델을 만들어 식수인원을 예측해보았습니다. 1 2 from google.colab import drive drive.mount('/content/drive') Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True). 1 2 3 4 5 6...","categories": ["Dacon"],
         "tags": ["python","Dacon","ml"],
         "url": "/dacon/dacon-first/",
         "teaser": null
       },{
         "title": "[Dacon] 와인 품질 분류 경진대회",
-        "excerpt":"데이콘 실무역량 강화교육 간에 실시한 프로젝트인 와인분류 프로젝트입니다. 성능 향상을 위해 앙상블기법을 활용해 보았습니다. 또한 하드보팅과 소프트 보팅에 대해 배웠습니다. 0.준비 1) 데이터 로드 from google.colab import drive drive.mount('/content/drive') Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True). import matplotlib.pyplot as plt import pandas as pd...","categories": ["Dacon"],
+        "excerpt":"데이콘 실무역량 강화교육 간에 실시한 프로젝트인 와인분류 프로젝트입니다. 성능 향상을 위해 앙상블기법을 활용해 보았습니다. 또한 하드보팅과 소프트 보팅에 대해 배웠습니다. 0.준비 1) 데이터 로드 1 2 from google.colab import drive drive.mount('/content/drive') 1 Drive already mounted at /content/drive; to attempt to forcibly remount, call drive.mount(\"/content/drive\", force_remount=True). 1 2 3 4 5...","categories": ["Dacon"],
         "tags": ["python","Dacon","ml"],
         "url": "/dacon/dacon-wine-classifier/",
         "teaser": null
       },{
         "title": "[Dacon] 유전체 정보 분류 경진대회",
-        "excerpt":"데이콘 유전체 정보 품종 분류 AI 경진대회 Import import pandas as pd import random import os import numpy as np import matplotlib.pyplot as plt from sklearn import preprocessing from sklearn.ensemble import RandomForestClassifier class CFG: SEED = 42 def seed_everything(seed): random.seed(seed) os.environ['PYTHONHASHSEED'] = str(seed) np.random.seed(seed) seed_everything(CFG.SEED) # Seed 고정 Data Load...","categories": ["Dacon"],
+        "excerpt":"데이콘 유전체 정보 품종 분류 AI 경진대회 Import 1 2 3 4 5 6 7 8 import pandas as pd import random import os import numpy as np import matplotlib.pyplot as plt from sklearn import preprocessing from sklearn.ensemble import RandomForestClassifier 1 2 class CFG: SEED = 42 1 2 3...","categories": ["Dacon"],
         "tags": ["python","Dacon","ml"],
         "url": "/dacon/dacon-gene/",
         "teaser": null
@@ -30,7 +30,7 @@ var store = [{
         "teaser": null
       },{
         "title": "[Dacon] 여행상품 신청예측",
-        "excerpt":"데이콘 Basic 여행상품 신청예측 from google.colab import drive drive.mount('/content/drive') Mounted at /content/drive 0.준비 import pandas as pd train = pd.read_csv('/content/drive/MyDrive/data/여행상품신청/train.csv') test = pd.read_csv('/content/drive/MyDrive/data/여행상품신청/test.csv') sample_submission = pd.read_csv('/content/drive/MyDrive/data/여행상품신청/sample_submission.csv') id : 샘플 아이디 Age : 나이 TypeofContact : 고객의 제품 인지 방법 (회사의 홍보 or 스스로 검색) CityTier : 주거 중인 도시의 등급....","categories": ["Dacon"],
+        "excerpt":"데이콘 Basic 여행상품 신청예측 1 2 from google.colab import drive drive.mount('/content/drive') 1 Mounted at /content/drive 0.준비 1 import pandas as pd 1 2 3 train = pd.read_csv('/content/drive/MyDrive/data/여행상품신청/train.csv') test = pd.read_csv('/content/drive/MyDrive/data/여행상품신청/test.csv') sample_submission = pd.read_csv('/content/drive/MyDrive/data/여행상품신청/sample_submission.csv') id : 샘플 아이디 Age : 나이 TypeofContact : 고객의 제품 인지 방법 (회사의 홍보 or 스스로...","categories": ["Dacon"],
         "tags": ["python","Dacon","ml"],
         "url": "/dacon/prediction-of-trevel-product/",
         "teaser": null
@@ -39,5 +39,17 @@ var store = [{
         "excerpt":"프로그래머스 코팅테스트 연습문제를 푸는 과정에서 굉장히 깔끔하게 풀린 문제가 있어서 풀이과정을 공유하고자 작성하게 되었다! 1. 문제 문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/43162 먼저 입력은 컴퓨터의 개수 n 과 컴퓨터 간 연결 관계에 대한 리스트 computers 가 주어지고 두 정보를 이용하여 네트워크의 개수 즉, 컴퓨터가 몇 묶음으로 연결되어있는지를 출력해야 한다. 2. 접근 방법 이...","categories": ["Programmers"],
         "tags": ["python","algorithm","dfs/bfs"],
         "url": "/programmers/programmers-network-solution/",
+        "teaser": null
+      },{
+        "title": "[keras 뿌시기] Sequential API",
+        "excerpt":"Sequential API 저는 지금 대학교에서 딥러닝 응용1 이라는 과목을 배우고 있는데, 강의에서 배운 내용을 복습도 하고 저만의 방식으로 배운 것을 기록을 하기 위해 이렇게 포스팅을 하게 되었습니다. 이미지 자료나 코드 모두 한경훈 교수님의 자료에서 참조하였으며, 한경훈 교수님의 블로그 링크를 첨부합니다. 블로그 링크 : https://sites.google.com/site/kyunghoonhan/keras?authuser=0 Mnist Data keras는 딥 러닝 모델을...","categories": ["Keras"],
+        "tags": ["python","keras","ml"],
+        "url": "/keras/keras_01/",
+        "teaser": null
+      },{
+        "title": "[keras 뿌시기] 자연어처리를 이용한 이진분류",
+        "excerpt":"IMDB 데이터셋을 활용한 이진분류 이미지 자료나 코드 모두 한경훈 교수님의 자료에서 참조하였으며, 한경훈 교수님의 블로그 링크를 첨부합니다. 블로그 링크 : https://sites.google.com/site/kyunghoonhan/keras?authuser=0 — IMDB 데이터셋 IMDB 데이터셋은 영화 리뷰에 대한 데이터로 리뷰가 긍정적인지 부정적인지에 대한 예측을 할 수 있습니다. 케라스의 tf.keras.datasets.imdb로부터 IMDB 데이터셋을 불러올 수 있습니다. 1 2 3 4 5...","categories": ["Keras"],
+        "tags": ["python","keras","ml"],
+        "url": "/keras/keras_02/",
         "teaser": null
       }]
